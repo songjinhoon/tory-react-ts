6fd95789ccbb667a63b60dc9bf2610cdee835fdf
songjinhoon <jinhoon0319@gmail.com>
2023-09-01 12:44:30 +0900

    feat: 테스트 버튼 및 기능 제거

--- src/page/dashboard/Dashboard.tsx
+++ src/page/dashboard/Dashboard.tsx
@@ -1,31 +1,20 @@
-import Header from '@component/layout/Header';
 import React, { useEffect } from 'react';
+import Header from '@component/layout/Header';
 import useUser from '@hook/useUser';
-import { useNavigate } from 'react-router-dom';
-import { useModeDispatch } from '../../context/mode';
 import ModalContainer from '@component/popup/modal/ModalContainer';
+import { useNavigate } from 'react-router-dom';
 import { useModalDispatch } from '../../context/modal';
 
 const Dashboard = () => {
   const { user, isLoading } = useUser();
   const navigate = useNavigate();
   const modalDispatch = useModalDispatch();
-  const modeDispatch = useModeDispatch();
 
   const sampleModalOpen = () =>
     modalDispatch({ type: 'addModal', value: 'sample' });
 
   const firstModalOpen = () =>
     modalDispatch({ type: 'addModal', value: 'first' });
-
-  const test = () => {
-    // actions?.addModal(modalType.sample);
-    modeDispatch({
-      type: 'updateMode',
-      key: 'userUpdateModal',
-      value: 'confirm',
-    });
-  };
 
   useEffect(() => {
     if (!isLoading && !user) {
@@ -44,7 +33,6 @@ const Dashboard = () => {
       <Header></Header>
       <button onClick={sampleModalOpen}>샘플 모달창 열기</button>
       <button onClick={firstModalOpen}>첫번째 모달창 열기</button>
-      <button onClick={test}>테스트버튼</button>
       <ModalContainer></ModalContainer>
     </div>
   );
