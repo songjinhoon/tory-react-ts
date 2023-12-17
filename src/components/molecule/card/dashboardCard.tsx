a8518445a8a49c4653dad4db360c56483a04a026
songjinhoon <jinhoon0319@gmail.com>
2023-12-17 09:20:07 +0900

    대시보드 소스 정리

---
+++ src/components/molecule/card/dashboardCard.tsx
@@ -0,0 +1,41 @@
+import BlogMain from '@assets/customBlogMain.gif';
+import PokemonMain from '@assets/customPokemonMain.gif';
+import { FC, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+interface Props {
+  type: 'blog' | 'pokemon';
+}
+
+const DashboardCard: FC<Props> = ({ type }) => {
+  const [isRotate, setIsRotate] = useState(false);
+  const navigate = useNavigate();
+
+  return (
+    <img
+      style={
+        isRotate
+          ? { transform: 'rotate(20deg)', width: 400, cursor: 'pointer' }
+          : {}
+      }
+      src={type === 'blog' ? BlogMain : PokemonMain}
+      alt={'card'}
+      width={300}
+      onMouseOver={() => {
+        setIsRotate((prevState) => !prevState);
+      }}
+      onMouseOut={() => {
+        setIsRotate((prevState) => !prevState);
+      }}
+      onClick={() => {
+        if (type === 'blog') {
+          alert('기능 준비중...');
+        } else {
+          navigate('/pokemon-field');
+        }
+      }}
+    />
+  );
+};
+
+export default DashboardCard;
--- src/components/organism/cardBox/dashboardCardBox.tsx
+++ src/components/organism/cardBox/dashboardCardBox.tsx
@@ -1,65 +1,12 @@
-import BlogMain from '@assets/customBlogMain.gif';
 import Block from '@components/molecule/block';
-import PokemonMain from '@assets/customPokemonMain.gif';
-import { useNavigate } from 'react-router-dom';
-import { useState } from 'react';
+import DashboardCard from '@components/molecule/card/dashboardCard';
 
 const DashboardCardBox = () => {
-  const navigate = useNavigate();
-  const [isRotate, setIsRotate] = useState({
-    blog: false,
-    pokemon: false,
-  });
-
   return (
     <>
-      <img
-        style={
-          isRotate.blog
-            ? { transform: 'rotate(20deg)', width: 400, cursor: 'pointer' }
-            : {}
-        }
-        src={BlogMain}
-        alt={'blogMain'}
-        width={300}
-        onMouseOver={() => {
-          setIsRotate({
-            ...isRotate,
-            blog: true,
-          });
-        }}
-        onMouseOut={() => {
-          setIsRotate({
-            blog: false,
-            pokemon: false,
-          });
-        }}
-        onClick={() => alert('기능 준비중...')}
-      />
+      <DashboardCard type={'blog'}></DashboardCard>
       <Block></Block>
-      <img
-        style={
-          isRotate.pokemon
-            ? { transform: 'rotate(20deg)', width: 400, cursor: 'pointer' }
-            : {}
-        }
-        src={PokemonMain}
-        alt={'pokemonMain'}
-        width={300}
-        onMouseOver={() => {
-          setIsRotate({
-            ...isRotate,
-            pokemon: true,
-          });
-        }}
-        onMouseOut={() => {
-          setIsRotate({
-            blog: false,
-            pokemon: false,
-          });
-        }}
-        onClick={() => navigate('/pokemon-field')}
-      />
+      <DashboardCard type={'pokemon'}></DashboardCard>
     </>
   );
 };
